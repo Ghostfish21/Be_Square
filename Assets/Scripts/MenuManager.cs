f16aa4cfde59fca165cf413b898dd96e84098ee9
oirbeboq <revamqq@gmail.com>
2024-11-12 20:10:40 -0500

    Shove Animation Completed

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -38,10 +38,12 @@ public class MenuManager : MonoBehaviour
     IEnumerator LoadScene()
     {
 
-        SceneCurtains.SetTrigger("SceneClose");
+        
         Player.SetTrigger("StartGame");
         //AudioManager.instance.PlaySFX(audioSearch.startGameSFX);
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(1.5f);
+        SceneCurtains.SetTrigger("SceneClose");
+        yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(SceneName);
     }
 
